20265dc2c935b72153038b399dd6f3a9fe593bf8
Ben Purinton <ben.purinton@gmail.com>
2023-02-07 18:58:10 +0000

    secret_encoder instructions

--- secret_encoder.rb
+++ secret_encoder.rb
@@ -1,8 +1,15 @@
-# Write a program that
-#   asks the user for a secret message
-#   and 'encode's the message by replacing vowels with other characters
-#   Here is our secret code, don't tell anyone... a = 1, e = 2, i = 3, o = 4, u = 5
-
+# Write a program that:
+#
+#   - Asks the user for a secret message (the starter code already does this part).
+#   - "Encodes" the message by replacing vowels with numbers.
+#   - Here is our secret code, don’t tell anyone…
+#
+#       - a should be replaced with 1
+#       - e should be replaced with 2
+#       - i should be replaced with 3
+#       - o should be replaced with 4
+#       - u should be replaced with 5
+#
 # Your program should print the encoded message.
 
 p "Enter in the secret you want to encode"
