c02eb46fc998858c0c197c7b8a002c9a8dcacf83
Ben Purinton <ben.purinton@gmail.com>
2023-02-07 19:20:05 +0000

    think_fast instructions

--- README.md
+++ README.md
@@ -30,33 +30,6 @@ If you are struggling, **try to experiment directly in the IRB environment** by 
 - think_fast
 - accumulate
 
-### think_fast.rb
-
-We have to expect the unexpected — if users can find a way to break our code by navigating through it in a sequence that we didn't predict, they 100% will.
-
-Suppose that your program has to deal with the object inside the variable `some_random_input`
-
-If the object is:
-
-- a String: downcase it and print it
-- a Time: figure out the day of the week, downcased, and print it
-- an Integer: figure out whether it's odd or even and print, `"X is even"` or `"X is odd"`
-- a Symbol: downcase it and print it
-- nil: print `"no object provided"`
-- true: print `"you may pass"`
-- false: print `"you may not pass"`
-- a Hash: print the list of keys within the Hash, as an Array.
-
-
-How would you start to go about it?
-
-Remember that, as developers, we're inventors — exploring the unknown and solving new problems that come our way. The aren't formulas to look up and follow for every situation; just tools in our belts. One of our most important tools for dealing with the unknown are `if` statements.
-
-https://chapters.firstdraft.com/chapters/763
-
-Next, remember that there's a method called `.class` that we can call on any Ruby object to find out what kind of thing it is. We first met it way back in the Integer Chapter, and we used it a lot especially when exploring an API response, which could come back to us in different shapes and sizes.
-
-
 ### accumulate.rb
 
 Add some code to this program so that is prints the history of answers given while being pestered whether we're there yet.
--- think_fast.rb
+++ think_fast.rb
@@ -1,20 +1,34 @@
-# Suppose that your program has to deal with the object inside the variable 'some_random_input'. If the object is:
-
-# a String: downcase it and print it
-# a Time: figure out the day of the week, downcased, and print
-# an Integer: figure out whether it's odd or even and print
-# a Symbol: downcase it and print it
-# nil: print "no object provided"
-# true: print "you may pass"
-# false: print "you may not pass"
-# a Hash: print the list of keys within the Hash, as an Array.
-
-# if statements are one of our most commonly used tools we use as developers for dealing with the unknown!
-#   Pull up the chapter if you need a refresher https://chapters.firstdraft.com/chapters/763
-
-# Next, remember that there's a method called `.class` that we can call on any Ruby object
-#   to find out what kind of thing it is. We first met it way back in the Integer Chapter,
-#   and we used it a lot especially when exploring an API response, which could come back to us in different shapes and sizes.
+# We have to expect the unexpected — if users can find a way 
+# to break our code by navigating through it in a sequence 
+# that we didn’t predict, they 100% will.
+#
+# Suppose that your program has to deal with the object inside 
+# the variable some_random_input
+#
+# If the object is:
+#
+#   - a String: downcase it and print it
+#   - a Time: figure out the day of the week, downcased, and print it
+#   - an Integer: figure out whether it’s odd or even and 
+#     print, "X is even" or "X is odd"
+#   - a Symbol: downcase it and print it
+#   - nil: print "no object provided"
+#   - true: print "you may pass"
+#   - false: print "you may not pass"
+#   - a Hash: print the list of keys within the Hash, as an Array.
+#
+# How would you start to go about it?
+#
+# Remember that, as developers, we’re inventors — exploring the unknown 
+# and solving new problems that come our way. The aren’t formulas to look 
+# up and follow for every situation; just tools in our belts. One of our 
+# most important tools for dealing with the unknown are if statements.
+#
+#     https://chapters.firstdraft.com/chapters/763
+#
+# Next, remember that there’s a method called .class that we can call on 
+# any Ruby object to find out what kind of thing it is. We first met it 
+# way back in the Integer Chapter
 
 unpredictable_inputs = [
   "Hello!",
